805165b517b4fd3a12dfcac47e94fcc3cece52c1
Gabriel <gbds1703farra@hotmail.com>
2022-05-30 18:52:46 -0300

    Iniciado Swagger na API.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'
+import swaggerUI from 'swagger-ui-express'
 import { routes } from './routes';
+import swaggerDocs from './swagger.json'
 
 dotenv.config();
 
@@ -9,7 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+app.use("/v1", routes);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log(`API execultando em http://localhost:${process.env.PORT || '3333'}`);
